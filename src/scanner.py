5f37f3875bf6eae3765865e8d3b41a2ac0ee2553
Mateusz Król <mateusz.krol@openx.com>
2024-10-14 17:34:32 +0200

    Finish lab 1

--- src/scanner.py
+++ src/scanner.py
@@ -1,4 +1,13 @@
-from sly import Lexer
+import sly
+
+
+Lexer = sly.Lexer
+
+class MyToken(sly.lex.Token):
+    def __repr__(self):
+        return f"({self.lineno}): {self.type}({self.value})"
+
+sly.lex.Token = MyToken
 
 
 class Scanner(Lexer):
@@ -8,68 +17,88 @@ class Scanner(Lexer):
         'for': 'FOR',
         'while': 'WHILE',
         'break': 'BREAK',
-        'continue ': 'CONTINUE',
+        'continue': 'CONTINUE',
         'return': 'RETURN',
         'eye': 'EYE',
         'zeros': 'ZEROS',
         'ones': 'ONES',
         'print': 'PRINT',
-
     }
 
-    # Set of token names.   This is always required
-    tokens = ["ID", ] + list(
-        reserved.values())
+    tokens = [
+        "PLUS", "MINUS", "TIMES", "DIVIDE",
+        "DOTPLUS", "DOTMINUS", "DOTTIMES", "DOTDIVIDE",
+        "ASSIGN", "ADDASSIGN", "SUBASSIGN", "MULASSIGN", "DIVASSIGN",
+        "EQ", "NEQ", "LE", "GE", "LT", "GT",
+        "LPAREN", "RPAREN", "LBRACKET", "RBRACKET", "LBRACE", "RBRACE",
+        "COLON", "TRANSPOSE", "COMMA", "SEMICOLON",
+        "ID", "INTNUM", "FLOATNUM", "STRING", "SKIP", "COMMENT"
+    ] + list(reserved.values())
 
-    # String containing ignored characters between tokens
-    ignore = ' \t'
+    literals = { '+', '-', '*', '/',
+                 '(', ')', '[', ']', '{', '}',
+                 ':', '\'', ',', ';' ,
+                 '='}
 
     # Regular expression rules for tokens
-    PLUS = r'\+'
-    MINUS = r'\-'
-    TIMES = r'\*'
-    DIVIDE = r'\/'
     DOTPLUS = r'\.\+'
     DOTMINUS = r'\.\-'
     DOTTIMES = r'\.\*'
     DOTDIVIDE = r'\.\/'
-    ASSIGN = r'='
-    PLUSEQ = r'\+='
-    MINUSEQ = r'\-='
-    TIMESEQ = r'\*='
-    DIVIDEEQ = r'\/='
+
+    ADDASSIGN = r'\+='
+    SUBASSIGN = r'\-='
+    MULASSIGN = r'\*='
+    DIVASSIGN = r'\/='
+
     EQ = r'=='
     NEQ = r'!='
     LE = r'<='
     GE = r'>='
     LT = r'<'
     GT = r'>'
-    LPAREN = r'\('
-    RPAREN = r'\)'
-    LBRACKET = r'\['
-    RBRACKET = r'\]'
-    LBRACE = r'\{'
-    RBRACE = r'\}'
-    COLON = r':'
-    TRANSPOSE = r"\'"
-    COMMA = r','
-    SEMICOLON = r';'
 
+    # Identifiers and keywords
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    INT = r'\d+'
-    FLOAT = r'\d+\.\d+([eE][-+]?\d+)?'
+    ID['if'] = 'IF'
+    ID['else'] = 'ELSE'
+    ID['for'] = 'FOR'
+    ID['while'] = 'WHILE'
+    ID['break'] = 'BREAK'
+    ID['continue'] = 'CONTINUE'
+    ID['return'] = 'RETURN'
+    ID['eye'] = 'EYE'
+    ID['zeros'] = 'ZEROS'
+    ID['ones'] = 'ONES'
+    ID['print'] = 'PRINT'
+
+    FLOATNUM = r'[-+]?(?:(?:\d*\.\d+)|(?:\d+\.\d*)|(?:\d+))(?:[eE][-+]?\d+)?'
+    INTNUM = r'\d+'
     STRING = r'\".*?\"|\'.*?\''
+
     SKIP = r'[ \t\n]+'
     COMMENT = r'\#.*'
-    MISMATCH = r'.'
 
-    KEYWORD = r'\b(?:if|else|for|while|break|continue|return|eye|zeros|ones|print)\b'
-    IDENTIFIER = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    @_(r'[-+]?(?:(?:\d*\.\d+)|(?:\d+\.\d*)|(?:\d+))(?:[eE][-+]?\d+)?')
+    def FLOATNUM(self, t):
+        t.value = float(t.value)
+        return t
+
+    @_(r'\d+')
+    def INTNUM(self, t):
+        t.value = int(t.value)
+        return t
+
+    @_(r'\#.*')
+    def COMMENT(self, t):
+        pass
+
+    @_(r'[ \t\n]+')
+    def SKIP(self, t):
+        self.lineno += t.value.count('\n')
+        pass
 
 
-
-
-
-
-
-
+    def error(self, t):
+        print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
+        self.index += 1
